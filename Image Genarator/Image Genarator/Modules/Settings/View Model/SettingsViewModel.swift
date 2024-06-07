11efef0ed0144e1d176b2fa2df63bfe3fb1bb86e
Aleksey Bidnyk <poveredbyme@gmail.com>
2024-06-07 18:48:39 +0300

    - Added settings view - Added manual switch for dark mode

---
+++ Image Genarator/Image Genarator/Modules/Settings/View Model/SettingsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewModel.swift
+//  Image Genarator
+//
+//  Created by Aleksey Bidnyk on 07.06.2024.
+//
+
+import Foundation
+import SwiftUI
+
+class SettingsViewModel: ObservableObject {
+        
+}
